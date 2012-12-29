096be88ff6e501b49d3aa60b0b41874eed277360
Olli-Pekka Heinisuo <o-p@unknownpixels.com>
2012-12-29 19:27:01 +0200

    Major refactoring, renamed class, sliced to smaller methods, enhanced error handling, optimized speed and removed unnessecary looping, added new folders and fixed several bugs

--- README.md
+++ README.md
@@ -4,28 +4,7 @@ This project's main goal is to offer an easy way to get food data from (Juvenes)
 
 I'm currently building an HTML5 web application on top of this.
 
-I'm not going to promise full uptime for the interface, but the data can be fetched from [relativity.fi](http://relativity.fi/juvenes/index.html)
-(Currently only in Finnish, documentation can be found there too)
-
-Since the code is open source, feel free to contribute or use it for your own purposes as long as licence conditions are met.
-
-## Changelog
-
-* 27.12.2012 Added preliminary support for TAY, TAMK and TAKK campuses
-
-## Usage
-
-Script creates by default ***food.json*** or ***ruoka.json*** file, depending on language.
-Example can be found from the repository.
-
-See the source and class initializer for example file path modification purposes etc.
-
-To run:
-`python json_food_TUT.py <language>`
-
-Language: fi or en, defaults to fi if no parameter given
-
-I recommend to use cron for scheluded runs.
+See full [DOCUMENTATION](http://skvark.github.com/TUT_Juvenes_food/).
 
 ## License
 
--- json_food_TUT.py
+++ json_food_TUT.py
@@ -26,10 +26,13 @@ import time
 # Outputs JSON-formatted files
 # See documentation for more specific info
 
-class TTYfood(object):
+# restaurant names, lookup dict
+restaurantNames = {0:'TUT', 1:'TAY', 2:'TAY Kauppi', 3:'TAMK', 4:'TAKK'}
+
+class JuvenesFood(object):
     """Creates nice JSON files about food and other general info of TTY Juvenes restaurants"""
     
-    def __init__(self, location, lang = 'fi'):
+    def __init__(self, location, restNames, lang = 'fi'):
     
         # start runtime logging
         self._start_time = time.time()
@@ -40,6 +43,18 @@ class TTYfood(object):
         
         # location
         self._location = location
+        
+        # error flag
+        self._error = False
+        
+        # decoded json data
+        self._loaded_json = ''
+        
+        # decoded restaurant info (opening hours etc.)
+        self._decoded_data_info = ''
+        
+        # temp dict
+        self._food = {}
         
         # inits dict with update time and error-flag
         # JSON time format: datetime.datetime.now().strftime('%Y-%m-%dT%H:%M:%S')
@@ -56,7 +71,7 @@ class TTYfood(object):
         # in KitchenId's place and the API returns some crazy html/css crap with opening hours in the middle :D
         
         # dict for the restaurants names, indices corresponding to the next list
-        self._restaurantNames = {0:'TUT', 1:'TAY', 2:'TAY Kauppi', 3:'TAMK', 4:'TAKK'}
+        self._restaurantNames = restNames
 
         self._restaurants = [
             # TUT
@@ -104,7 +119,118 @@ class TTYfood(object):
         self._opener = urllib2.build_opener()
         self._opener.addheaders = [('User-agent', 'Mozilla/5.0')]
         self._default_timeout = 20
+
+    def fetch_food(self):
+        """Loops trough two weeks, day by day, restaurant by restaurant via this API 
+        http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx
+        and returns info as a dictionary"""
         
+        try: # check for misconfig etc.
+        
+            self._logger.info(' Starting '+self._restaurantNames[self._location]+'...')
+            for w in range(0,2): # loop for weeks
+            
+                # initializing dicts
+                self._food = {}
+                # hack for the week 52 and next year
+                # though Juvenes API gets fucked up when week 52 and next yers week 1..
+                if self._week == 52 and w == 1:
+                    curweek = 1
+                else:
+                    curweek = self._week+w
+                    
+                self._info[curweek] = {}
+                self._logger.info(' Starting week '+str(curweek)+'...')
+                
+                for i in range(0,6): # weekdays
+                    self._food[i] = {}
+                    
+                    for restaurant in self._restaurants[self._location]:
+                        
+                        restr = {}
+                        fetch_params = { # setting the GET params
+                            'kitchen': restaurant['kitchen'],
+                            'menutype': restaurant['menutype'],
+                            'weekday': i+1,
+                            'week': curweek,
+                            'kitcheninfoid': restaurant['kitcheninfoid'],
+                            'lang': self._language
+                        }
+                        self._error = False
+                        self._decoded_data_info = ''
+                        
+                        self._get_json_from_juvenes(restaurant, fetch_params, i) # get the json data
+                        
+                        if self._error == True:
+                           continue # jump back to the beginning of the loop   
+                        else:
+                            restra = self._get_restaurants_foods(restr) # omomommmsmsmsm
+                            self._food[i][restaurant['restaurant']] = restra # saving restaurant specific food to dict
+                            
+                            try:
+                                pvm = self._loaded_json['MealOptions'][0]['MenuDate']
+                                self._food[i]['date'] = pvm # saving date to dict
+                            except:
+                                self._food[i]['date'] = ''  # if not found, save empty date
+                        
+                        if i == 0 and self._decoded_data_info != '': # if "monday", parse and save the opening hours
+                        
+                            json_info = json.loads(self._decoded_data_info.strip()[1:-2])
+                            s = self._parse_opening_hours(str(json_info['d']))
+                            self._food[restaurant['restaurant']+'_open'] = s
+                            
+                self._info[curweek] = self._food # save the restaurant dict to the week-key at the end of the week
+        except:
+            self._info['fatal_error'] = 'True'
+       
+        # execution time
+        self._end_time = time.time()
+        self._logger.info(' Language was '+self._language+'.') # lang info
+        self._logger.info(' Fetch time: '+instance._get_execution_time()+'.')
+        
+        return self._info # class returns food dictionary
+    
+    # gets the json from Juvenes site
+    def _get_json_from_juvenes(self, restaurant, fetch_parameters, i):
+        
+        try:
+            fetched_data = self._opener.open('http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetMenuByWeekday?KitchenId=%(kitchen)s&MenuTypeId=%(menutype)s&Week=%(week)d&Weekday=%(weekday)d&lang=\'%(lang)s\'&format=json' % fetch_parameters)
+            decoded_data = fetched_data.read().decode('utf-8')
+            
+            # stripping some stuff out from the "json"
+            decoded_data = json.loads(decoded_data.strip()[1:-2])['d']
+            if decoded_data.endswith(');'):
+                decoded_data.strip()[:-2]
+            self._loaded_json = json.loads(decoded_data)
+
+            if i == 0: # this gets the opening hours at every "monday" for the week (they are the same whole week)
+                fetched_data_info = self._opener.open('http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetKitchenInfo?KitchenInfoId=%(kitcheninfoid)s&lang=\'%(lang)s\'&format=json' % fetch_parameters)
+                self._decoded_data_info = fetched_data_info.read().decode('utf-8')
+
+        except: # if there's some problem when fetching the data, this raises the error flag
+            self._food['error'] = 'Some data could not be fetched.'
+            self._logger.info(' Error in '+restaurant['restaurant']+'.')
+            self._error = True
+    
+    # gets the food from current restaurant
+    def _get_restaurants_foods(self, restr):
+        """Loops trough the foods"""
+        
+        # loops the food names until exception raises
+        for j in range( 0, len(self._loaded_json['MealOptions'])):
+                        
+            food_type = self._loaded_json['MealOptions'][j]['Name']
+            try:
+                restr[food_type] = self._loaded_json['MealOptions'][j]['MenuItems'][0]['Name']
+            except IndexError:
+                continue
+                
+            # loops all the potatoes, rice and other related stuff :D (until exception)
+            for h in range(1, len(self._loaded_json['MealOptions'][j]['MenuItems']) ):
+                restr[food_type] += ', '+self._loaded_json['MealOptions'][j]['MenuItems'][h]['Name']
+
+        return restr
+
     def _parse_opening_hours(self, data_string):
         """Parses some spaghetti html and inline css off and 
         returns nicely formatted opening hours"""
@@ -118,127 +244,41 @@ class TTYfood(object):
         s = h.unescape(s)                                       # unescapes html entities and returns unicode
         return s
         
-    def fetch_food(self):
-        """Loops trough two weeks, day by day, restaurant by restaurant via this API 
-        http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx
-        and returns info as a dictionary"""
-        self._logger.info(' Starting '+self._restaurantNames[self._location]+'...')
-        for w in range(0,2): # loop for weeks
-        
-            # initializing dicts
-            food = {}
-            
-            # hack for the week 52 and next year
-            # though Juvenes API gets fucked up when week 52 and next yers week 1..
-            if self._week == 52 and w == 1:
-                curweek = 1
-            else:
-                curweek = self._week+w
-                
-            self._info[curweek] = {}
-            self._logger.info(' Starting week '+str(curweek)+'...')
-            
-            for i in range(0,6): # weekdays
-                food[i] = {}
-                pvm = ''
-                for restaurant in self._restaurants[self._location]:
-                
-                    restr = {}
-                    fetch_parameters = { # setting the GET params
-                        'kitchen': restaurant['kitchen'],
-                        'menutype': restaurant['menutype'],
-                        'weekday': i+1,
-                        'week': curweek,
-                        'kitcheninfoid': restaurant['kitcheninfoid'],
-                        'lang': self._language
-                    }
-                    error = False
-                    decoded_data_info = ''
-                    try:
-                    
-                        fetched_data = self._opener.open('http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetMenuByWeekday?KitchenId=%(kitchen)s&MenuTypeId=%(menutype)s&Week=%(week)d&Weekday=%(weekday)d&lang=\'%(lang)s\'&format=json' % fetch_parameters)
-                        decoded_data = fetched_data.read().decode('utf-8')
-                        
-                        # stripping some stuff out from the "json"
-                        decoded_data = json.loads(decoded_data.strip()[1:-2])['d']
-                        if decoded_data.endswith(');'):
-                            decoded_data.strip()[:-2]
-                        loaded_json = json.loads(decoded_data)
-                        
-                        if i == 0: # this gets the opening hours at every "monday" for the week (they are the same whole week)
-                            fetched_data_info = self._opener.open('http://www.juvenes.fi/DesktopModules/Talents.LunchMenu/LunchMenuServices.asmx/GetKitchenInfo?KitchenInfoId=%(kitcheninfoid)s&lang=\'%(lang)s\'&format=json' % fetch_parameters)
-                            decoded_data_info = fetched_data_info.read().decode('utf-8')
-                                                
-                    except: # if there's some problem when fetching the data, this raises the error flag
-                        food['error'] = 'True'
-                        self._logger.info(' Error in '+restaurant['restaurant']+'.')
-                        error = True
-                    
-                    try:
-                        pvm = loaded_json['MealOptions'][0]['MenuDate'] # get the date
-                        
-                        for j in range(0,4): # loops the food names until exception raises
-                        
-                            type = loaded_json['MealOptions'][j]['Name']
-                            restr[type] = loaded_json['MealOptions'][j]['MenuItems'][0]['Name']
-                            
-                            try:
-                                for h in range(1,3): # loops all the potatoes, rice and other related stuff :D (until exception)
-                                    restr[type] += ', '+loaded_json['MealOptions'][j]['MenuItems'][h]['Name']
-                            except:
-                                pass # we don't care
-                    except:
-                        pass # we don't care
-                        
-                    food[i][restaurant['restaurant']] = restr # saving restaurant specific food to dict
-                    food[i]['date'] = pvm # saving date to dict
-                    
-                    if i == 0 and decoded_data_info != '': # if "monday", parse and save the opening hours
-                    
-                        json_info = json.loads(decoded_data_info.strip()[1:-2])
-                        s = self._parse_opening_hours(str(json_info['d']))
-                        food[restaurant['restaurant']+'_open'] = s
-
-            self._info[curweek] = food # save the restaurant dict to the week-key at the end of the week
-        self._end_time = time.time()
-        self._logger.info(' Language was '+self._language+'.')
-        self._logger.info(' Fetch time: '+instance._get_execution_time()+'.')
-        return self._info # class return food dictionary
-        
     def _get_execution_time(self):
         """Returns execution time"""
     
-        return "%.2f" % (self._end_time-self._start_time, )
+        return "%.2f" % (self._end_time - self._start_time, )
         
-# function for writing the dict into an JSON file.        
-def write_json(language, location, food_dict):
+# Function for writing the dict into an JSON file.        
+def write_json(language, location, food_dict, restNames):
     """Writes JSON to file"""
-    
-    restaurantNames = {0:'TUT', 1:'TAY', 2:'TAY Kauppi', 3:'TAMK', 4:'TAKK'}
-    
+     
     # sorts keys and adds indent for more pretty output
     data = json.dumps(food_dict, sort_keys=True, indent=4)
         
     if lang == 'fi':
-        f = open('ruoka_'+restaurantNames[location]+'.json', 'w+') # overwrite old
+        f = open('data/'+language+'/ruoka_'+restNames[location]+'.json', 'w+') # overwrite old
     else:
-        f = open('food_'+restaurantNames[location]+'.json', 'w+') # overwrite old
+        f = open('data/'+language+'/food_'+restNames[location]+'.json', 'w+') # overwrite old
             
     f.write(data + "\n")        # save to file
     f.close()
-    print "Wrote JSON to file."
+    print "Wrote "+restNames[location]+" food info to JSON file."
 
 # usage example
 # accepts "fi" and "en" as parameters
-
 if __name__ == "__main__":
+
     if len(sys.argv) > 1:
         lang = sys.argv[1]
         if lang != 'en' and  lang != 'fi':
             exit('Parameter error. Parameter was: '+lang+' Accepted values: none, fi, en')
     else:    
         lang = 'fi'
-    for location in range(0,5):
-        instance = TTYfood(location, lang) # inits object
+        
+    # loops trough different campuses, inits len(restaurantNames) instances
+    for location in range( 0, len(restaurantNames) ):
+    
+        instance = JuvenesFood(location, restaurantNames, lang) # inits object
         food = instance.fetch_food() # fetches the stuff
-        write_json(lang, location, food) # writes json
+        write_json(lang, location, food, restaurantNames) # writes json
